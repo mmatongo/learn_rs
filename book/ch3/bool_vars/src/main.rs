2faea8938f77ec4a1bf458318f2ee4ddd269c136
Daniel M. Matongo <mmatongo_@hotmail.com>
2022-11-01 15:44:51 +0000

    bool_vars

---
+++ book/ch3/bool_vars/src/main.rs
@@ -0,0 +1,22 @@
+fn main() {
+    let truth = true;
+    let lie = false;
+    println!("truth is {}", truth);
+    println!("lie is {}", lie);
+
+    // but we can also use arithmetic to determine this
+    let truth = 5 > 2;
+    let lie = -3 >= 8;
+
+    println!("truth: {} - false: {}", truth, lie);
+
+    /*
+     * ==: is equal to
+     * !=: is different from
+     * <: is less than
+     * <=: is less than or equal to
+     * >: is greater than
+     * >=: is greater than or equal to
+     */
+
+}
---
+++ book/ch3/bool_vars/target/debug/bool_vars.d
@@ -0,0 +1,1 @@
+/home/dm/work/learn_rs/book/ch3/bool_vars/target/debug/bool_vars: /home/dm/work/learn_rs/book/ch3/bool_vars/src/main.rs
